e570cf60e75e31c9c4d804c976b7ea16d7e202f2
knockknockyoo <iheaven0129@gmail.com>
2023-12-06 15:01:34 -0500

    1. validate function 2. Test codes including test cases

--- src/metadata_validator.py
+++ src/metadata_validator.py
@@ -8,7 +8,7 @@ from bento.common.sqs import VisibilityExtender
 from bento.common.utils import get_logger
 from bento.common.s3 import S3Bucket
 from common.constants import BATCH_STATUS, BATCH_TYPE_METADATA, DATA_COMMON_NAME, ERRORS, DB, \
-    ERRORS, S3_DOWNLOAD_DIR, SQS_NAME, SCOPE, MODEL, SUBMISSION_ID
+    S3_DOWNLOAD_DIR, SQS_NAME, SCOPE, MODEL, SUBMISSION_ID, PASSED, ERROR
 from common.utils import cleanup_s3_download_dir, get_exception_msg, dump_dict_to_json
 from common.model_store import ModelFactory
 from data_loader import DataLoader
@@ -19,7 +19,7 @@ def metadataValidate(configs, job_queue, mongo_dao):
     batches_processed = 0
     log = get_logger('Metadata Validation Service')
     try:
-        model_store = ModelFactory(configs) 
+        model_store = ModelFactory(configs)
         # dump models to json
         dump_dict_to_json([model[MODEL] for model in model_store.models], f"tmp/data_models_dump.json")
     except Exception as e:
@@ -33,7 +33,7 @@ def metadataValidate(configs, job_queue, mongo_dao):
         try:
             log.info(f'Waiting for jobs on queue: {configs[SQS_NAME]}, '
                             f'{batches_processed} batches have been processed so far')
-            
+
             for msg in job_queue.receiveMsgs(VISIBILITY_TIMEOUT):
                 log.info(f'Received a job!')
                 extender = None
@@ -47,12 +47,12 @@ def metadataValidate(configs, job_queue, mongo_dao):
                         scope = data[SUBMISSION_ID]
                         submissionID = data[SUBMISSION_ID]
                         result = validator.validate(submissionID, scope)
-                    
+
                     else:
                         log.error(f'Invalid message: {data}!')
 
                     batches_processed +=1
-                    
+
                 except Exception as e:
                     log.debug(e)
                     log.critical(
@@ -79,7 +79,7 @@ Compose a list of files to be updated and their sizes (metadata or files)
 """
 
 class MetaDataValidator:
-    
+
     def __init__(self, configs, mongo_dao, model_store):
         self.configs = configs
         self.fileList = [] #list of files object {file_name, file_path, file_size, invalid_reason}
@@ -109,40 +109,53 @@ class MetaDataValidator:
         #2. loop through all records and call validateNode
 
         #3. update records
-        
+
         return True
 
-    
+
     def validateNode(self, dataRecord, model):
         self.validate_required_props(dataRecord, model)
         return None
 
     def validate_required_props(self, data_record, node_definition):
-
         result = {"result": ERROR, "errors": [], "warnings": []}
+        # check the correct format from the node_definition
+        if "model" not in node_definition.keys() or "nodes" not in node_definition["model"].keys():
+            result["errors"].append(f"node definition is not correctly formatted.")
+            return result
+        # check the correct format from the data_record
+        if "nodeType" not in data_record.keys() or "rawData" not in data_record.keys() or len(data_record["rawData"].items()) == 0:
+            result["errors"].append(f"data record is not correctly formatted.")
+            return result
 
-
+        # validation start
         nodes = node_definition["model"]["nodes"]
+        node_type = data_record["nodeType"]
         # extract a node from the data record
-        if data_record["nodeType"] not in nodes.keys():
-            result["errors"].append(f"Required property '{data_record['nodeType']}' is missing or empty.")
-        else:
-            anode = nodes[data_record["nodeType"]]
+        if node_type not in nodes.keys():
+            result["errors"].append(f"Required node '{node_type}' does not exist.")
+            return result
 
-        if data_record["nodeType"] in nodes.keys():
+        anode = nodes[node_type]
+        if node_type in nodes.keys():
             for key, value in data_record["rawData"].items():
-                if 'properties' not in anode:
-                    key_not_exist = key not in anode.properties
-                    value_invalid = not value.strip()
-                    if key_not_exist or value_invalid:
-                        result["errors"].append(f"Required property '{key}' is missing or empty.")
+                anode_keys = anode.keys()
+                if "properties" not in anode_keys:
+                    result["errors"].append(f"data record is not correctly formatted.")
+                    break
 
-        result["result"] = ERROR if result["errors"] else PASSED
+                key_not_exist = key not in anode["properties"]
+                value_invalid = not value.strip()
+                if key_not_exist or value_invalid:
+                    result["errors"].append(f"Required property '{key}' is missing or empty.")
+
+        if len(result["errors"]) == 0:
+            result["result"] = PASSED
         return result
 
     def validate_prop_value(self, dataRecord, model):
         return None
-    
+
     def validate_relationship(self, dataRecord, model):
         return None
 
