602edc348aae4805c0e2141ac14330c00df36b3e
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-02-04 15:10:02 -0500

    rollback the change on search release

--- src/common/mongo_dao.py
+++ src/common/mongo_dao.py
@@ -843,13 +843,7 @@ class MongoDao:
         db = self.client[self.db_name]
         data_collection = db[RELEASE_COLLECTION]
         try:
-            result = data_collection.find_one({DATA_COMMON_NAME: data_commons, NODE_TYPE: node_type, NODE_ID: node_id, 
-                "$expr": {
-                "$in": [
-                  { "$arrayElemAt": ["$status", -1] }, # Get the last element of the array
-                  status                                # Check if it equals nodeStatus
-                ]
-              }})
+            result = data_collection.find_one({DATA_COMMON_NAME: data_commons, NODE_TYPE: node_type, NODE_ID: node_id, SUBMISSION_REL_STATUS: {"$in": status}})
             return list(result) if result else None
         except errors.PyMongoError as pe:
             self.log.exception(pe)
