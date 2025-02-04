0ff9d201d88df163cbd4b72445f4a37afe060b86
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-02-04 13:43:34 -0500

    optimized.

--- mongo_db_script/restoreApplicationVersion.js
+++ mongo_db_script/restoreApplicationVersion.js
@@ -1,14 +1,7 @@
 // Use the appropriate database
 // use crdc-datahub2;
-
-// Define the function to get the code based on title and description
-function getVersionByStatus(status) {
-    const currentVersion = "1.0";
-    const newStatusVersion = "2.0"; 
-    return ["New", "In Progress", "Inquired"].includes(status) ? newStatusVersion : currentVersion;
-}
   
-// Function to process and update a collection (errors or warnings)
+// Function to restore application version based on status
 function restoreApplicationVersion() {
     const bulkOps = [];
     let matchedCount = 0;
@@ -17,16 +10,15 @@ function restoreApplicationVersion() {
     print("----------------------");
     console.log(`${new Date()} -> Restoring data field: "version"`);
     db.applications.find({ "version": {$exists: false}, "status": {$exists: true, $ne: null} }).forEach(doc => {
-    matchedCount++;
-    doc["version"] = getVersionByStatus(doc["status"]);
-    bulkOps.push({
-        updateOne: {
-            filter: { _id: doc._id },
-            update: { $set: {"version": doc["version"] } }
-        }
-    });
-    updatedCount++;
-
+        matchedCount++;
+        doc["version"] = ["New", "In Progress", "Inquired"].includes(doc["status"]) ? "2.0" : "1.0";
+        bulkOps.push({
+            updateOne: {
+                filter: { _id: doc._id },
+                update: { $set: {"version": doc["version"] } }
+            }
+        });
+        updatedCount++;
     });
 
     if (bulkOps.length > 0) {
