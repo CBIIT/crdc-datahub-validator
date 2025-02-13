6e681584d210c4b8329ff1dec0266c5310e8811c
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-02-13 16:16:43 -0500

    update

--- src/dcf_manifest_generator.py
+++ src/dcf_manifest_generator.py
@@ -43,7 +43,7 @@ class GenerateDCF:
         if control_access == True and not dbGaPID:
             self.log.error(f'If control access is set true, dbGaPID is required!')
             return 
-        dbGaPID = dbGaPID.split('.')[0]
+        dbGaPID = dbGaPID.split('.')[0] if dbGaPID else None
         acl ="['*']" if not control_access else f"['{dbGaPID}']"
         authz = "['/open']" if not control_access else f"['/programs/{dbGaPID}']"
         url =  f's3://{self.config[PROD_BUCKET_CONFIG_NAME]}/{self.submission.get(STUDY_ID)}/'
