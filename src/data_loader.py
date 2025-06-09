e5c6d45933fc7f17218a392a2d40aa7c6343d76d
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2025-06-09 16:52:32 -0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -265,8 +265,9 @@ class DataLoader:
     """
     def get_file_info(self, type, prop_names, row):
         file_fields = self.file_nodes.get(type)
-        file_name = row.get(SUBFOLDER_FILE_NAME)
-        if file_name is None or file_name == "": 
+        if row.get(SUBFOLDER_FILE_NAME) is not None and row.get(SUBFOLDER_FILE_NAME) != "":
+            file_name = row.get(SUBFOLDER_FILE_NAME)
+        else: 
             file_name = row[file_fields[FILE_NAME_FIELD]] if file_fields[FILE_NAME_FIELD] in prop_names else None
         file_size = row[file_fields[FILE_SIZE_FIELD]] if file_fields[FILE_SIZE_FIELD] in prop_names else None
         file_md5 = row[file_fields[FILE_MD5_FIELD]] if file_fields[FILE_MD5_FIELD] in prop_names else None
