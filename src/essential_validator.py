aa721e35f608d3caca0682b79871f9277d1e9c04
Ming Ying <yingming18@gmail.com>
2025-10-09 17:54:25 -0400

    Only raise the error if any key properties are missing.

--- src/essential_validator.py
+++ src/essential_validator.py
@@ -438,19 +438,20 @@ class EssentialValidator:
         composition_key = self.model.get_composition_key(type)
         # validate composition key properties
         if composition_key:
-            # raise an error if any composite key required columns are missing
-            if not all(prop in columns for prop in composition_key):
-                msg = f'“{file_info[FILE_NAME]}”: Columns {json.dumps(composition_key)} are required to generate composite ID.'
-                self.log.error(msg)
-                file_info[ERRORS].append(msg)
-                self.batch[ERRORS].append(msg)
-                return False
+
             # loop through all rows and check if all properties in composition key (array) values are empty row by row
             for index, row in self.df.iterrows():
                 hasVal = False
                 if not pd.isnull(row[id_field]):
                     continue
                 for prop in composition_key:
+                    # raise error if any columns required by the composite key is not present
+                    if prop not in columns:
+                        msg = f'“{file_info[FILE_NAME]}”: Column "{prop}" is required to generate composite ID.'
+                        self.log.error(msg)
+                        file_info[ERRORS].append(msg)
+                        self.batch[ERRORS].append(msg)
+                        return False
                     if not pd.isnull(row[prop]):
                         hasVal = True
                         break
